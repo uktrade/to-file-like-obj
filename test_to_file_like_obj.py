8677405ce493a5adf811116f5c9db98750874c1f
Michal Charemza <michal@charemza.name>
2023-11-13 07:00:40 +0000

    tests: assert that the length is well respected

--- test_to_file_like_obj.py
+++ test_to_file_like_obj.py
@@ -10,6 +10,13 @@ def test_basic():
     assert b''.join(iter(lambda: f.read(1), b'')) == b''.join(bytes_iter)
 
 
+def test_well_behaved():
+    bytes_iter = (b'ab', b'cd', b'ef')
+    f = to_file_like_obj(bytes_iter)
+
+    assert list(iter(lambda: len(f.read(1)), 0)) == [1, 1, 1, 1, 1, 1]
+
+
 def test_textiowrapper_groups_into_lines():
     bytes_iter = (b'a\nb', b'c\nd', b'e\nf')
     f = to_file_like_obj(bytes_iter)
