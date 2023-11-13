97bb33b1c273afbafdb34017825c8d639e943e51
Michal Charemza <michal@charemza.name>
2023-11-13 06:56:56 +0000

    tests: assert that TextIOWrapper groups into lines

--- test_to_file_like_obj.py
+++ test_to_file_like_obj.py
@@ -1,3 +1,5 @@
+import io
+
 from to_file_like_obj import to_file_like_obj
 
 
@@ -6,3 +8,11 @@ def test_basic():
 	f = to_file_like_obj(bytes_iter)
 
 	assert b''.join(iter(lambda: f.read(1), b'')) == b''.join(bytes_iter)
+
+
+def test_textiowrapper_groups_into_lines():
+	bytes_iter = (b'a\nb', b'c\nd', b'e\nf')
+	f = to_file_like_obj(bytes_iter)
+
+	lines = io.TextIOWrapper(f, newline='', encoding='utf=8')
+	assert list(lines) == ['a\n', 'bc\n', 'de\n', 'f']
