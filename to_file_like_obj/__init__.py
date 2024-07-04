78411f0a5522934d1a14486bc8c1ab7b58871215
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2024-07-04 20:47:54 +0100

    adds last type

--- to_file_like_obj/__init__.py
+++ to_file_like_obj/__init__.py
@@ -1,12 +1,11 @@
 from io import IOBase
-from typing import Iterable, Type
+from typing import Iterable, Type, Iterator
 from typing_extensions import Buffer
-
 
 def to_file_like_obj(iterable: Iterable[bytes], base: Type[bytes]=bytes) -> IOBase:
     chunk: bytes = base()
     offset: int = 0
-    it = iter(iterable)
+    it: Iterator[bytes] = iter(iterable)
 
     def up_to_iter(size: int) -> Iterable[Buffer]:
         nonlocal chunk, offset
