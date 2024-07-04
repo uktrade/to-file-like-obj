aff172e4058f9ecba9523b9bccd588d6c4ea7780
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2024-07-04 21:09:58 +0100

    adds most typing
    adds final types and modifies infinity
    
    adds last type
    
    adds spacing

--- to_file_like_obj/__init__.py
+++ to_file_like_obj/__init__.py
@@ -1,12 +1,14 @@
 from io import IOBase
+from typing import Iterable, Type, Iterator
+from typing_extensions import Buffer
 
 
-def to_file_like_obj(iterable, base=bytes):
-    chunk = base()
-    offset = 0
-    it = iter(iterable)
+def to_file_like_obj(iterable: Iterable[bytes], base: Type[bytes]=bytes) -> IOBase:
+    chunk: bytes = base()
+    offset: int = 0
+    it: Iterator[bytes] = iter(iterable)
 
-    def up_to_iter(size):
+    def up_to_iter(size: int) -> Iterable[Buffer]:
         nonlocal chunk, offset
 
         while size:
@@ -17,18 +19,19 @@ def to_file_like_obj(iterable, base=bytes):
                     break
                 else:
                     offset = 0
-            to_yield = min(size, len(chunk) - offset)
+            to_yield: int = min(size, len(chunk) - offset)
             offset = offset + to_yield
             size -= to_yield
             yield chunk[offset - to_yield : offset]
 
     class FileLikeObj(IOBase):
-        def readable(self):
+        def readable(self) -> bool:
             return True
 
-        def read(self, size=-1):
+        def read(self, size: int=-1) -> bytes:
+            max_size: int = 2**63 - 1
             return base().join(
-                up_to_iter(float('inf') if size is None or size < 0 else size)
+                up_to_iter(max_size if size is None or size < 0 else size)
             )
 
     return FileLikeObj()
