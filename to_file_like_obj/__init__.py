eb12f4154ccf826cb73a7686a9079385cd193ab7
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2024-07-05 15:31:45 +0100

    adds type annotations

--- to_file_like_obj/__init__.py
+++ to_file_like_obj/__init__.py
@@ -1,12 +1,26 @@
 from io import IOBase
+from typing import TypeVar, Iterable, Type, Iterator, overload
 
+T = TypeVar('T', str, bytes)
 
-def to_file_like_obj(iterable, base=bytes):
+@overload
+def to_file_like_obj(iterable: Iterable[bytes]) -> IOBase:
+    ...
+
+@overload
+def to_file_like_obj(iterable: Iterable[bytes], base: Type[bytes]) -> IOBase:
+    ...
+
+@overload
+def to_file_like_obj(iterable: Iterable[str], base: Type[str]) -> IOBase:
+    ...
+
+def to_file_like_obj(iterable: Iterable[T], base=bytes) -> IOBase:
     chunk = base()
-    offset = 0
+    offset: int = 0
     it = iter(iterable)
 
-    def up_to_iter(size):
+    def up_to_iter(size: int) -> Iterator[T]:
         nonlocal chunk, offset
 
         while size:
@@ -17,18 +31,19 @@ def to_file_like_obj(iterable, base=bytes):
                     break
                 else:
                     offset = 0
-            to_yield = min(size, len(chunk) - offset)
+            to_yield: int = min(size, len(chunk) - offset)
             offset = offset + to_yield
             size -= to_yield
             yield chunk[offset - to_yield : offset]
 
     class FileLikeObj(IOBase):
-        def readable(self):
+        def readable(self) -> bool:
             return True
 
-        def read(self, size=-1):
+        def read(self, size: int=-1) -> T:
+            max_size: int = 2**63 - 1
             return base().join(
-                up_to_iter(float('inf') if size is None or size < 0 else size)
+                up_to_iter(max_size if size is None or size < 0 else size)
             )
 
     return FileLikeObj()
